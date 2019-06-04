708a4d0ca74f9c9e1e892416ddf63987bdf6cf84
Timothy <thuang87@berkeley.edu>
2019-06-03 21:53:18 -0700

    Created CourseCategory model, defined many-to-one relationship from Course to CourseCategory, and created api endpoint for getCourseCategory

--- course/views.py
+++ course/views.py
@@ -1,10 +1,18 @@
 from django.http import JsonResponse
 
 from course.models import Course
+from course.models import CourseCategory
 
 
 def get_catalog(request):
-	courses = list(Course.objects.values())
-	return JsonResponse({
-		"data": courses,
-	})
+    courses = list(Course.objects.values())
+    return JsonResponse({
+        "data": courses,
+    })
+
+
+def getCourseCategories(request):
+    courseCategories = list(CourseCategory.objects.values())
+    return JsonResponse({
+        "data": courseCategories
+    })
