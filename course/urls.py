97653d289148fd18e83bec7c194536e936abf769
Timothy <thuang87@berkeley.edu>
2019-06-09 18:29:14 -0700

    did minor refactoring and added new view endpoint for getting courses on category input

--- course/urls.py
+++ course/urls.py
@@ -4,5 +4,7 @@ from course import views
 
 urlpatterns = [
     path('catalog/', views.get_catalog, name='catalog'),
-    path('catalog/categories', views.get_course_categories, name='catalog'),
+    path('catalog/categories', views.get_course_categories, name='categories'),
+    path('catalog/categories/<category_name>',
+         views.get_category_courses, name='category-courses')
 ]
--- course/views.py
+++ course/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 
 from course.models import Course
@@ -7,7 +8,7 @@ from course.models import CourseCategory
 def get_catalog(request):
     courses = list(Course.objects.values())
     return JsonResponse({
-        "data": courses,
+        "data": courses
     })
 
 
@@ -15,4 +16,11 @@ def get_course_categories(request):
     course_categories = list(CourseCategory.objects.values())
     return JsonResponse({
         "data": course_categories
+    })
+
+
+def get_category_courses(request, category_name):
+    course_list = get_list_or_404(Course, course_category=category_name)
+    return JsonResponse({
+        "data": course_list
     })
