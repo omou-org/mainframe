3b73498b70bc26bbb19f9324db6725f330c5ad3d
Timothy <thuang87@berkeley.edu>
2019-06-30 14:38:06 -0700

    fixed typo

--- account/serializers.py
+++ account/serializers.py
@@ -110,6 +110,6 @@ class InstructorSerializer(serializers.ModelSerializer):
         new_user = User.objects.create(username=user_data['email'], password="omou12312",
                                        email=user_data['email'], first_name=user_data['first_name'], last_name=user_data['last_name'])
 
-        parent = Parent.objects.create(user=new_user, **validated_data)
+        instructor = Instructor.objects.create(user=new_user, **validated_data)
 
-        return parent
+        return instructor
