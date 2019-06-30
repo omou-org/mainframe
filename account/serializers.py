74fea536e1b1ace7e67289d089be333994ee8767
Timothy <thuang87@berkeley.edu>
2019-06-30 14:04:21 -0700

    changed admin email

--- account/serializers.py
+++ account/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from account.models import (
     Student,
     Parent,
@@ -6,15 +7,23 @@ from account.models import (
 from rest_framework import serializers
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+
+
 class StudentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Student
         read_only_fields = (
             'updated_at',
             'created_at',
+            'user'
         )
         fields = (
-            'user',
             'gender',
             'address',
             'city',
@@ -26,6 +35,16 @@ class StudentSerializer(serializers.ModelSerializer):
             'school',
             'parent'
         )
+
+    def create(self, validated_data):
+        User = get_user_model()
+
+        user_data = validated_data.pop('user')
+
+        # new_user = User.objects.create(username=user_data.get('email'), password="asdf124124",
+        #                                email=user_data.get('email'), first_name=user_data.get('first_name'), last_name=user_data.get('last_name'))
+
+        # return new_user
 
 
 class ParentSerializer(serializers.ModelSerializer):
