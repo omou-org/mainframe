7eddc7f82e72c36a276cda61c1fb7bcd00734faa
Jerry Li <jerry.li@robinhood.com>
2019-11-10 12:50:59 -0800

    [models] Fix serializers

--- account/serializers.py
+++ account/serializers.py
@@ -176,6 +176,7 @@ class ParentSerializer(serializers.ModelSerializer):
             'state',
             'zipcode',
             'relationship',
+            'secondary_phone_number',
             'updated_at',
             'created_at',
         )
