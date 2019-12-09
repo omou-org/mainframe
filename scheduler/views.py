6eb9b84edfee1a40bb016dffd62e297245ccef82
Jerry Li <jerrylinew@gmail.com>
2019-12-08 21:02:20 -0800

    add filter by type of course

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,3 +1,4 @@
+
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
@@ -8,8 +9,10 @@ from scheduler.serializers import (
     SessionSerializer
 )
 
+from course.models import Course
 from mainframe.permissions import IsDev, ReadOnly
 from scheduler.models import Session
+from scheduler.serializers import
 
 
 class SessionViewSet(viewsets.ModelViewSet):
@@ -19,7 +22,10 @@ class SessionViewSet(viewsets.ModelViewSet):
     def list(self, request):
         time_frame = request.query_params.get("time_frame", None)
         view_option = request.query_params.get("view_option", None)
-
         queryset = self.get_queryset()
+        if view_option == "class":
+            queryset = queryset.filter(course__type=Course.CLASS)
+        if view_option == "tutoring":
+            queryset = queryset.filter(course__type=Course.TUTORING)
         serializer = self.get_serializer_class()(queryset, many=True)
         return Response(serializer.data)
