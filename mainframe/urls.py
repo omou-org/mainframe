bf776859d558fc53641ca44bce05f51a69ef112f
Daniel Huang <danielhuang@g.ucla.edu>
2019-12-11 22:30:46 -0800

    basic pricing app and model

--- mainframe/urls.py
+++ mainframe/urls.py
@@ -23,6 +23,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('pricing/', include('pricing.urls')),
     path('search/', include('search.urls')),
     path('course/', include('course.urls')),
     path('account/', include('account.urls')),
---
+++ pricing/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from pricing.models import Price
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from mainframe.permissions import IsDev, ReadOnly
+
+from pricing.serializers import PriceSerializer
+
+
+# Create your views here.
+class PriceViewSet(viewsets.ModelViewSet):
+    """"
+    API endpoint that allows prices to be viewed, edited, or created
+    """
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsDev | (IsAuthenticated & (IsAdminUser | ReadOnly))]
+    queryset = Price.objects.all()
+    serializer_class = PriceSerializer
+
