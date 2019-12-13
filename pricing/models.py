2c9e0f26d0749ff0e624b2085145201a3d7f9d38
Jerry Li <jerry.li@robinhood.com>
2019-12-12 21:52:53 -0800

    [pricing] fix pricing stuff

--- pricing/models.py
+++ pricing/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from course.models import CourseCategory
 
-# Create your models here.
-class Price(models.Model):
+
+class PriceRule(models.Model):
     # Basic price information
     name = models.CharField(
         max_length=1000,
@@ -10,18 +10,12 @@ class Price(models.Model):
         null=True,
     )
     hourly_tuition = models.DecimalField(max_digits=5, decimal_places=2)
-
-    class Meta:
-        abstract = True
-
-class PriceRule(Price):
-    # Price Rule fields
     category = models.ForeignKey(
-        CourseCategory, on_delete=models.PROTECT, null=True)
-    ELEMENTARY_LVL = "ELEMENTARY_LVL"
-    MIDDLE_LVL = "MIDDLE_LVL"
-    HIGH_LVL = "HIGH_LVL"
-    COLLEGE_LVL = "COLLEGE_LVL"
+        CourseCategory, on_delete=models.PROTECT, blank=True, null=True)
+    ELEMENTARY_LVL = "elementary_lvl"
+    MIDDLE_LVL = "middle_lvl"
+    HIGH_LVL = "high_lvl"
+    COLLEGE_LVL = "college_lvl"
 
     ACADEMIC_CHOICES = (
         (ELEMENTARY_LVL, "Elementary"),
@@ -30,7 +24,7 @@ class PriceRule(Price):
         (COLLEGE_LVL, "College"),
     )
     academic_level = models.CharField(
-        max_length=10,
+        max_length=20,
         choices=ACADEMIC_CHOICES,
         blank=True,
         null=True,
@@ -38,8 +32,6 @@ class PriceRule(Price):
     min_capacity = models.IntegerField(null=True, blank=True)
     max_capacity = models.IntegerField(null=True, blank=True)
 
-class StaticPrice(Price):
-    # List of associated courses with the tuition
-    @property
-    def course_list(self):
-        return [course_id for course_id in self.course_set.all()]
+    # Timestamps
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
