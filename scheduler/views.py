96b700f360064a85f94aa279371d477a11a319c6
Jerry Li <jerry.li@robinhood.com>
2019-12-23 00:34:56 -0800

    [scheduler] Add modified list and delete for sessions

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
@@ -29,13 +29,40 @@ class SessionViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(course__type=Course.TUTORING)
 
         now = datetime.now()
+        start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
         if time_frame == "day":
-            start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
+            end_of_day = start_of_day + timedelta(days=1)
             queryset = queryset.filter(
-                start_datetime__gte=start_of_day)
+                start_datetime__gte=start_of_day,
+                end_datetime__lt=end_of_day
+            )
         elif time_frame == "week":
-            pass
+            start_of_week = start_of_day - timedelta(days=(start_of_day.weekday() + 1) % 7)
+            end_of_week = start_of_week + timedelta(days=7)
+            queryset = queryset.filter(
+                start_datetime__gte=start_of_week,
+                end_datetime__lt=end_of_week
+            )
         elif time_frame == "month":
-            pass
+            start_of_month = start_of_day.replace(day=1)
+            if start_of_month.month == 12:
+                end_of_month = start_of_month.replace(month=1)
+            else:
+                end_of_month = start_of_month.replace(month=start_of_month.month + 1)
+            queryset = queryset.filter(
+                start_datetime__gte=start_of_month,
+                end_datetime__lt=end_of_month
+            )
         serializer = self.get_serializer_class()(queryset, many=True)
         return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        course_id = request.query_params.get("course_id", None)
+        if course_id is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        now = datetime.now()
+        self.get_queryset().filter(
+            course=course_id,
+            end_datetime__gt=now
+        ).delete()
+        return Response(status.HTTP_204_NO_CONTENT)
