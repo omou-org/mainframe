7ff3fbee4029731a46bd45a3b3e6035fcf544270
Matthew Li <firescapecal@gmail.com>
2020-07-21 22:23:25 -0700

    log pricing app

--- pricing/mutations.py
+++ pricing/mutations.py
@@ -18,7 +18,7 @@ from pricing.schema import (
 from course.mutations import AcademicLevelEnum, CourseTypeEnum
 
 from graphql_jwt.decorators import login_required, staff_member_required
-from django.contrib.admin.models import LogEntry, ADDITION
+from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
@@ -32,11 +32,11 @@ class AmountTypeEnum(graphene.Enum):
 class CreatePriceRule(graphene.Mutation):
     class Arguments:
         rule_id = graphene.ID(name='id')
-        name = graphene.String(required=True)
-        hourly_tuition = graphene.Float(required=True)
-        category_id = graphene.Int(name="category", required=True)
-        academic_level = AcademicLevelEnum(required=True)
-        course_type = CourseTypeEnum(required=True)
+        name = graphene.String()
+        hourly_tuition = graphene.Float()
+        category_id = graphene.Int(name="category")
+        academic_level = AcademicLevelEnum()
+        course_type = CourseTypeEnum()
     
     price_rule = graphene.Field(PriceRuleType)
     created = graphene.Boolean()
@@ -45,9 +45,9 @@ class CreatePriceRule(graphene.Mutation):
     @staff_member_required
     def mutate(root, info, **validated_data):
         existing_rules = PriceRule.objects.filter(
-            Q(category=validated_data['category_id']) &
-            Q(academic_level=validated_data['academic_level']) &
-            Q(course_type=validated_data['course_type']))
+            Q(category=validated_data.get('category_id')) &
+            Q(academic_level=validated_data.get('academic_level')) &
+            Q(course_type=validated_data.get('course_type')))
         if 'rule_id' not in validated_data and existing_rules.count() > 0:
             raise GraphQLError('Failed mutation. PriceRule already exists.')
 
@@ -56,10 +56,17 @@ class CreatePriceRule(graphene.Mutation):
             if not PriceRule.objects.filter(id=rule_id).exists():
                 raise GraphQLError('Failed update mutation. PriceRule does not exist.')
         price_rule, created = PriceRule.objects.update_or_create(
-            id=validated_data.pop('rule_id', None),
+            id=validated_data.get('rule_id', None),
             defaults=validated_data
         )
 
+        LogEntry.objects.log_action(
+            user_id=info.context.user.id,
+            content_type_id=ContentType.objects.get_for_model(PriceRule).pk,
+            object_id=price_rule.id,
+            object_repr=str(price_rule.id),
+            action_flag=CHANGE if 'rule_id' in validated_data else ADDITION
+        )
         return CreatePriceRule(price_rule=price_rule, created=created)
 
 
@@ -76,6 +83,14 @@ class DeletePriceRule(graphene.Mutation):
             price_rule_obj = PriceRule.objects.get(id=validated_data.get('id'))
         except ObjectDoesNotExist:
             raise GraphQLError('Failed delete mutation. PriceRule does not exist.')
+        
+        LogEntry.objects.log_action(
+            user_id=info.context.user.id,
+            content_type_id=ContentType.objects.get_for_model(PriceRule).pk,
+            object_id=price_rule_obj.id,
+            object_repr=str(price_rule_obj.id),
+            action_flag=DELETION
+        )
         price_rule_obj.delete()
         return DeletePriceRule(deleted=True)
 
@@ -94,6 +109,14 @@ class CreateDiscount(graphene.Mutation):
     @staff_member_required
     def mutate(root, info, **validated_data):
         discount = Discount.objects.create(**validated_data)
+
+        LogEntry.objects.log_action(
+            user_id=info.context.user.id,
+            content_type_id=ContentType.objects.get_for_model(Discount).pk,
+            object_id=discount.id,
+            object_repr=discount.name,
+            action_flag=ADDITION
+        )
         return CreateDiscount(discount=discount)
 
 
@@ -112,6 +135,14 @@ class CreateMultiCourseDiscount(graphene.Mutation):
     @staff_member_required
     def mutate(root, info, **validated_data):
         multi_course_discount = MultiCourseDiscount.objects.create(**validated_data)
+
+        LogEntry.objects.log_action(
+            user_id=info.context.user.id,
+            content_type_id=ContentType.objects.get_for_model(MultiCourseDiscount).pk,
+            object_id=multi_course_discount.id,
+            object_repr=multi_course_discount.name,
+            action_flag=ADDITION
+        )
         return CreateMultiCourseDiscount(multi_course_discount=multi_course_discount)
 
 
@@ -131,6 +162,14 @@ class CreateDateRangeDiscount(graphene.Mutation):
     @staff_member_required
     def mutate(root, info, **validated_data):
         date_range_discount = DateRangeDiscount.objects.create(**validated_data)
+
+        LogEntry.objects.log_action(
+            user_id=info.context.user.id,
+            content_type_id=ContentType.objects.get_for_model(DateRangeDiscount).pk,
+            object_id=date_range_discount.id,
+            object_repr=date_range_discount.name,
+            action_flag=ADDITION
+        )
         return CreateDateRangeDiscount(date_range_discount=date_range_discount)
 
 
@@ -149,6 +188,14 @@ class CreatePaymentMethodDiscount(graphene.Mutation):
     @staff_member_required
     def mutate(root, info, **validated_data):
         payment_method_discount = PaymentMethodDiscount.objects.create(**validated_data)
+
+        LogEntry.objects.log_action(
+            user_id=info.context.user.id,
+            content_type_id=ContentType.objects.get_for_model(PaymentMethodDiscount).pk,
+            object_id=payment_method_discount.id,
+            object_repr=payment_method_discount.name,
+            action_flag=ADDITION
+        )
         return CreatePaymentMethodDiscount(payment_method_discount=payment_method_discount)
 
 
