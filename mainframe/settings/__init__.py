a5a9563bdb778808cd715c64f23589230687bd65
Jerry Li <jerry.li@robinhood.com>
2020-07-24 23:09:01 -0700

    [settings] Move cors middleware up

--- mainframe/settings/__init__.py
+++ mainframe/settings/__init__.py
@@ -74,13 +74,13 @@ COMMON_APPS = (
 INSTALLED_APPS = API_APPS + COMMON_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
