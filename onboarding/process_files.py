e69290c3f5621b74fb5e41b9ba50330b5c3db4dd
rg <ryan@gedwill.com>
2020-12-05 13:59:21 -0800

    validation

--- onboarding/process_files.py
+++ onboarding/process_files.py
@@ -1,2 +1,49 @@
-def process_import(path):
-    return path
+from openpyxl import load_workbook
+
+def process_accounts(sheet):
+    pass
+
+def process_categories(sheet):
+    pass
+
+def process_courses(sheet):
+    pass
+
+def process_import(path, file_type):
+    wb = load_workbook(filename=path)
+    sheet = wb.active
+    last_col = sheet.max_column
+    sheets = wb.sheetnames
+    print('sheet name' , sheets)
+    sheet_ranges = sheet['A3'].value
+    return (path, file_type)
+
+column_names = {
+    "students": [
+        'first name',
+        'last name',
+        'email',
+        'date of birth',
+        'school',
+        'grade',
+        'parent first name',
+        'parent email'
+    ],
+    "parent" : [
+        "first name",
+        "last name",
+        "email",
+        "phone",
+        "zip code"
+    ],
+    "instructors" : [
+        "first name",
+        "last name",
+        "email",
+        "phone",
+        "date of birth",
+        "teaching subjects",
+        "biography",
+        "years of experience"
+    ]
+}
