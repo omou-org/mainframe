b855a4cb3bbb7ef99b6c4734a7ba03f307ec1ccd
Matthew Li <firescapecal@gmail.com>
2021-03-23 21:01:29 -0700

    save

--- invoice/mutations.py
+++ invoice/mutations.py
@@ -1,4 +1,4 @@
-from django.contrib.admin.models import LogEntry, ADDITION
+from django.contrib.admin.models import LogEntry, ADDITION, CHANGE
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 
@@ -7,7 +7,7 @@ import stripe
 from graphene import Field, ID, Int, List, String, Float, Boolean
 from graphql import GraphQLError
 
-from account.models import Parent
+from account.models import Admin, Parent
 from course.models import Enrollment
 from invoice.models import Invoice, RegistrationCart
 from invoice.serializers import InvoiceSerializer
@@ -24,14 +24,15 @@ class EnrollmentQuote(graphene.InputObjectType):
 
 class CreateInvoice(graphene.Mutation):
     class Arguments:
+        invoice_id = ID()
         method = String(required=True)
         disabled_discounts = List(ID)
         price_adjustment = Float()
         classes = List(ClassQuote)
         tutoring = List(TutoringQuote)
-        parent = ID(required=True)
+        parent = ID()
         registrations = List(EnrollmentQuote)
-        payment_status = PaymentChoiceEnum()
+        payment_status = PaymentChoiceEnum(required=True)
     
     invoice = Field(InvoiceType)
     stripe_connected_account = String()
@@ -42,8 +43,8 @@ class CreateInvoice(graphene.Mutation):
     @login_required
     def mutate(root, info, **validated_data):
         data = validated_data
-        data.update(price_quote_total(data))    
-        
+        data.update(price_quote_total(data))
+
         discounts = data.pop("discounts")
         data["deductions"] = []
         for discount in discounts:
@@ -54,10 +55,31 @@ class CreateInvoice(graphene.Mutation):
                 }
             )
 
-        serializer = InvoiceSerializer(data=data, context={'user_id': info.context.user.id})
-        serializer.is_valid(raise_exception=True)
-        invoice = serializer.save()
+        # update invoice
+        if data.get('invoice_id'):
+            invoice = Invoice.objects.get(id=data.get('invoice_id'))
+            serializer = InvoiceSerializer(invoice, data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+           
+            invoice.refresh_from_db()
+            operation = CHANGE
+        else:
+            serializer = InvoiceSerializer(data=data, context={'user_id': info.context.user.id})
+            serializer.is_valid(raise_exception=True)
+            invoice = serializer.save()
+            operation = ADDITION
 
+        # log operation
+        LogEntry.objects.log_action(
+            user_id=info.context.user.id,
+            content_type_id=ContentType.objects.get_for_model(Invoice).pk,
+            object_id=invoice.id,
+            object_repr=f"{invoice.parent.user.first_name} {invoice.parent.user.last_name}, {invoice.method}",
+            action_flag=operation
+        )
+
+        # stripe integration
         stripe_checkout_id = None
         if validated_data['method'] == 'credit_card':
             stripe.api_key = settings.STRIPE_API_KEY
@@ -81,13 +103,6 @@ class CreateInvoice(graphene.Mutation):
             )
             stripe_checkout_id = session.id
 
-        LogEntry.objects.log_action(
-            user_id=info.context.user.id,
-            content_type_id=ContentType.objects.get_for_model(Invoice).pk,
-            object_id=invoice.id,
-            object_repr=f"{invoice.parent.user.first_name} {invoice.parent.user.last_name}, {invoice.method}",
-            action_flag=ADDITION
-        )
         return CreateInvoice(
             invoice=invoice,
             stripe_connected_account='acct_1HqSAYETk4EmXsx3',
