e25bfba6339e8587a2d39ecf8e882b0a75414697
Jerry Li <jerry.li@robinhood.com>
2021-04-25 22:46:09 -0700

    fix bug

--- account/schema.py
+++ account/schema.py
@@ -107,7 +107,9 @@ class Query(object):
     user_info = Field(UserInfoType, user_id=ID(), user_name=String())
     user_type = Field(UserTypeAuth, user_id=ID(), user_name=String(), admin_types=Boolean())
     email_from_token = Field(String, token=String())
-    verify_google_oauth_token = Field(GoogleVerifyTokenType, login_email=String(required=True))
+    verify_google_oauth_token = Field(
+        GoogleVerifyTokenType, login_email=String(required=True), oauth_email=String(required=True)
+    )
 
     account_notes = List(AccountNoteType, user_id=ID(required=True))
     students = List(StudentType, grade=ID())
