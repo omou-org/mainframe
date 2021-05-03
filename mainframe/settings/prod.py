5a6ca6439f5ef735d89718eb8714227bfe216425
Jerry Li <jerry.li@robinhood.com>
2021-05-02 19:12:03 -0700

    fix password

--- mainframe/settings/prod.py
+++ mainframe/settings/prod.py
@@ -22,7 +22,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['RDS_DB_NAME'],
         'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': os.environ['RDS_HOSTNAME'],
         'PORT': os.environ['RDS_PORT'],
     },
