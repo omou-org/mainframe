58486d593b3aeaa66dd278809ebc9c14236b55e1
Jerry Li <jerry.li@robinhood.com>
2021-05-08 10:20:11 -0700

    fix names

--- mainframe/settings/prod.py
+++ mainframe/settings/prod.py
@@ -21,8 +21,8 @@ DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['DATABASE_USERNAME'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
         'HOST': os.environ['RDS_HOSTNAME'],
         'PORT': os.environ['RDS_PORT'],
     },
