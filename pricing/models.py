3e17d91729f1ed417223a45bf7fa1a509469251b
Michael Li <ml5885@pleasantonusd.net>
2021-07-08 17:10:19 -0700

    [business_data][discounts] - created new combined discount model and migration

--- pricing/models.py
+++ pricing/models.py
@@ -68,18 +68,14 @@ class TuitionPrice(models.Model):
 
 class Discount(models.Model):
     # Basic discount information
-    name = models.CharField(
+    code = models.CharField(
         max_length=1000,
         blank=True,
     )
-    description = models.CharField(
-        max_length=1000,
-        blank=True,
-    )
-    amount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    auto_apply = models.BooleanField(default=False)
+    
     PERCENT = "percent"
     FIXED = "fixed"
-
     AMOUNT_CHOICES = (
         (PERCENT, "percent"),
         (FIXED, "fixed"),
@@ -89,8 +85,40 @@ class Discount(models.Model):
         choices=AMOUNT_CHOICES,
         default=FIXED,
     )
+    amount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
 
     active = models.BooleanField(default=True)
+
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+
+    min_courses = models.IntegerField(
+        validators=[MinValueValidator(2), MaxValueValidator(1000)], 
+        blank=True, 
+        null=True,
+    )  
+
+    CASH = "cash"
+    CREDIT_CARD = "credit_card"
+    COURSE_CREDIT = "course_credit"
+    CHECK = "check"
+    INTL_CREDIT_CARD = "intl_credit_card"
+    METHOD_CHOICES = (
+        (CASH, "Cash"),
+        (COURSE_CREDIT, "Course Credit"),
+        (CREDIT_CARD, "Credit Card"),
+        (CHECK, "Check"),
+        (INTL_CREDIT_CARD, "International Credit Card"),
+    )
+    payment_method = models.CharField(
+        max_length=20,
+        choices=METHOD_CHOICES,
+        null=True,
+        blank=True,
+    )
+
+    all_courses_apply = models.BooleanField(default=True)
+    courses = models.ManyToManyField("course.course", blank=True)
 
     # Timestamps
     updated_at = models.DateTimeField(auto_now=True)
