820977a07cd8843b3024bc4727f7bcfb49658e37
Calvin Fronda <cfronda209@gmail.com>
2021-07-09 12:47:42 -0700

    feat: get_instructor_sessions
    Pseudo code for get_instructor_sessions.

--- scheduler/utilities.py
+++ scheduler/utilities.py
@@ -6,6 +6,10 @@ from account.models import (
     InstructorOutOfOffice,
 )
 from onboarding.models import Business
+from scheduler.models import Session
+from django.utils.timezone import make_aware
+from django.utils import timezone
+from course.models import Course
 
 
 def time_to_index(time: datetime, day_of_week: str, business_id: int) -> int:
@@ -27,27 +31,81 @@ def index_to_time(index: int, day_of_week: str, business_id: int) -> datetime:
     today = datetime.today()
     business_start_time = (datetime.combine(today, start_time)).time()
     minutes = business_start_time.hour * 60 + business_start_time.minute
-
     total_minutes = minutes + (30 * index)
     final_time = timedelta(minutes=total_minutes)
     return final_time
 
 
+def duration_to_boolean_list(start_time, end_time):
+    today = datetime.today()
+    start_time = datetime.combine(today, start_time)
+    end_time = datetime.combine(today, end_time)
+    difference = end_time - start_time
+    total_minutes = difference.total_seconds() / 60
+    index = total_minutes / 30
+    return [True] * int(index)
+
+
 def get_buessiness_hours_map(buesiness_id: int) -> dict:
-    # create a function that returns a dict of days as keys and array of bools for the amount of hours in a day
+    availability_dict = {}
     business = Business.objects.get(id=buesiness_id)
-    avail = business.availability_list
-    r = avail
-    print(r)
+    avail = business.availability_list.all()
+
+    for e in avail:
+        if e.day_of_week not in availability_dict:
+            availability_dict[e.day_of_week] = duration_to_boolean_list(
+                e.start_time, e.end_time
+            )
+
+    return availability_dict
 
 
-def set_instructor_availabilities(
-    instructor_id: int, instructor_availiablities_map: dict
-) -> dict:
+def get_instructor_sessions(instructor_id: int, start_date, end_date) -> list:
+    weekdays = {
+        0: "monday",
+        1: "tuesday",
+        2: "wednesday",
+        3: "thursday",
+        4: "friday",
+        5: "saturday",
+        6: "sunday",
+    }
+    instructor_availabilities_dict = {}
+    instructor_sessions = Session.objects.filter(
+        start_datetime__range=(start_date, end_date),
+        end_datetime__range=(start_date, end_date),
+        instructor_id=instructor_id,
+    )
+
+    # Need to get start & end time of course
+    instructor_availability = InstructorAvailability.instructor
+    print(instructor_availability.instructor)
+
+    for e in instructor_sessions:
+        if weekdays[e.start_datetime.weekday()] not in instructor_availabilities_dict:
+            instructor_availabilities_dict[weekdays[e.start_datetime.weekday()]] = 1
+
+    print(instructor_availabilities_dict)
+
+
+def set_instructor_business_availabilities(instructor_id: int) -> dict:
     pass
 
 
-print(get_buessiness_hours_map(buesiness_id=1))
-# day = time(10, 30)
-# print(index_to_time(9, "monday", 1))
-# print(time_to_index(day, "monday", 1))
+def get_instructor_availabilities(instructor_id: int, start_date, end_date) -> dict:
+    # given an instructor ID and a start & end time
+    # return an object with possible availabilities
+    # get business hours map
+    #
+    pass
+
+
+st = time(10, 0)
+et = time(11, 00)
+sd = datetime(2020, 1, 26, 10, 0, tzinfo=timezone.utc)
+ed = datetime(2021, 10, 15, 11, 0, tzinfo=timezone.utc)
+# print(get_instructor_availabilities(2, sd, ed))
+print(get_instructor_sessions(5, sd, ed))
+# print(get_buessiness_hours_map(buesiness_id=1))
+# print(index_to_time(1, "monday", 1))
+# print(time_to_index(selected_time, "monday", 1))
